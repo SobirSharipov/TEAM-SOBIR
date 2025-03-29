79b7e7af4bffbce7ab80a6ff3eee86b9660acbad
karimwo7 <kurbanovkarim61@gmail.com>
2025-03-29 13:20:28 +0500

    no bag

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,10 @@
 / @type {import('tailwindcss').Config} */
-<<<<<<< HEAD
- export default {
+ export default { 
   content: ["./src//*.{html,js}"],
   theme: {
     extend: {},
     screens: {
-=======
-export default {
-   content: ["./src//*.{html,js}"],
-   theme: {
-     extend: {},
-     screens: {
-<<<<<<< HEAD
-=======
-
->>>>>>> 4d291ed645698267df31f19ce8d6f0094b3d5a5d
-      'xs': '0px',   // Phones
-      'md': '510px',   // ipad
-      'lg': '1280px',  // Desktops
-
-<<<<<<< HEAD
-     },
-  },
-   plugins: [],
-}
-=======
-  },
-   },
-   plugins: [],
- }
->>>>>>> 4d291ed645698267df31f19ce8d6f0094b3d5a5d
->>>>>>> 69ab880454575d6a2680fe6986837aec0219f6c3
-
+ 
       'xs': '0px',
       'md': '510px',
       'lg': '1280px',
